9a10f3a39b9eea142a8706fd51038b692bbf3079
Melit26 <yuligonzalez26@gmail.com>
2022-07-21 12:31:24 +0200

    App Component y Cabecero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
+import { EgresoService } from './egreso/egreso.service';
+import { IngresoService } from './ingreso/ingreso.service';
 import { Component } from '@angular/core';
+import { Ingreso } from './ingreso/ingreso.model';
+import { Egreso } from './egreso/egreso.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'presupuesto-app';
+
+  ingresos: Ingreso[]=[];
+  egresos: Egreso[]=[];
+
+  constructor(private ingresoService: IngresoService,
+              private egresoService: EgresoService){
+                this.ingresos = ingresoService.ingresos;
+                this.egresos = egresoService.egresos;
+              }
+
+  getIngresoTotal() {
+    let ingresoTotal:number=0
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal += ingreso.valor;
+    });
+    return ingresoTotal
+  }
+
+  getEgresoTotal() {
+    let egresoTotal:number=0
+    this.egresos.forEach(egreso => {
+      egresoTotal += egreso.valor;
+    }
+    );
+    return egresoTotal
+  }
+
+  getPorcentajeTotal() {
+    return this.getEgresoTotal()/this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal() {
+    return this.getIngresoTotal()-this.getEgresoTotal();
+  }
+
 }
--- src/app/ingreso/ingreso.component.ts
+++ src/app/ingreso/ingreso.component.ts
@@ -1,4 +1,6 @@
+import { IngresoService } from './ingreso.service';
 import { Component, OnInit } from '@angular/core';
+import { Ingreso } from './ingreso.model';
 
 @Component({
   selector: 'app-ingreso',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresoComponent implements OnInit {
 
-  constructor() { }
+  ingresos:Ingreso[]=[];
 
-  ngOnInit(): void {
+  constructor(private ingresoService:IngresoService) { }
+
+  ngOnInit() {
+    this.ingresos = this.ingresoService.ingresos;
   }
 
 }
